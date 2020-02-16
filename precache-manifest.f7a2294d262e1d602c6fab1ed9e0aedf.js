8309a29dae4d35b12f69276235ed214dc3c2bba6
tgc <tuogengchen@gmail.com>
2020-02-15 21:03:31 -0500

    Updates

---
+++ precache-manifest.f7a2294d262e1d602c6fab1ed9e0aedf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa37e7ae3f002f6b5b7b98bd87f043b3",
+    "url": "/currency_converter/index.html"
+  },
+  {
+    "revision": "bbed9dacf862d6592866",
+    "url": "/currency_converter/static/css/main.0873c653.chunk.css"
+  },
+  {
+    "revision": "f6c1cbd606c64ea76b5c",
+    "url": "/currency_converter/static/js/2.63ce82f8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/currency_converter/static/js/2.63ce82f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbed9dacf862d6592866",
+    "url": "/currency_converter/static/js/main.96ccd310.chunk.js"
+  },
+  {
+    "revision": "bf8dc17c4a7b0e6a262d",
+    "url": "/currency_converter/static/js/runtime-main.72e9049b.js"
+  }
+]);
